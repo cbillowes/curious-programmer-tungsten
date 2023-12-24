01c0f9b01079316b6d5d9d0ec6857cd862257ce1
Clarice Bouwer <cbillowes@gmail.com>
2023-12-24 12:40:24 +0400

    Comment out unused Search component

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Search from '@components/search';
+// import Search from '@components/search';
 import Logo from '@components/logo';
 import ThemeSwitcher from '@components/theme-switcher';
 
