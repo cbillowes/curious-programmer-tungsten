6ae66225e901957171f116f1e71b83a518d12214
Clarice Bouwer <cbillowes@gmail.com>
2023-12-24 13:10:13 +0400

    Force center aligned unicorn on spinner

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -5,7 +5,7 @@ const Spinner = () => {
     <div className="loader-wrapper fixed top-0 bottom-0 left-0 right-0 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-pink-700 via-pink-800 to-gray-900 z-[9999] text-white text-2xl tracking-tight">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
         <img
-          className="w-64 animate-bouncy"
+          className="w-64 animate-bouncy mx-auto inline-block"
           src={require('@images/unicorn-coffee.webp').default}
         />
         <br />
