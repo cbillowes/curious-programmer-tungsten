c0029e1799b9579185348213d63f50466aea2f36
Clarice Bouwer <cbillowes@gmail.com>
2023-12-24 13:06:33 +0400

    Primary color kebab on light theme

--- src/components/kebab.js
+++ src/components/kebab.js
@@ -7,7 +7,7 @@ const Kebab = ({ prefix, onClick, className, children, phrases, expanded }) => {
     <div
       className={classNames(
         'max-w-2xl justify-center mx-auto mt-2 flex flex-wrap relative w-full py-2 rounded-md shadow-md',
-        expanded ? " bg-primary-600 text-white hover:bg-blue-600" : "dark:bg-gray-800 bg-gray-100 hover:bg-primary-600 hover:dark:bg-primary-600 hover:text-white",
+        expanded ? " bg-primary-600 text-white hover:bg-blue-600" : "dark:bg-gray-800 bg-primary-200 hover:bg-primary-600 hover:dark:bg-primary-600 hover:text-white",
         className,
       )}
       onClick={onClick}
