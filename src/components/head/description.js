1665d28733c187463860293ea703f7ac5b651c19
Clarice Bouwer <cbillowes@gmail.com>
2023-12-27 11:33:52 +0400

    Shorten SEO description

--- src/components/head/description.js
+++ src/components/head/description.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-const Description = ({ keywords, description, author }) => (
-  <>
-    <meta content={author} name="author" />
-    <meta content={author} name="publisher" />
-    <meta content={keywords} name="keywords" />
-    <meta content={description} name="description" />
-    <meta content={description} property="og:description" />
-    <meta content={description} name="twitter:description" />
-  </>
-);
+const Description = ({ keywords, description, author }) => {
+  const shortened =
+    description.length > 160
+      ? description?.substring(0, 157) + '...'
+      : description;
+  return (
+    <>
+      <meta content={author} name="author" />
+      <meta content={author} name="publisher" />
+      <meta content={keywords} name="keywords" />
+      <meta content={shortened} name="description" />
+      <meta content={shortened} property="og:description" />
+      <meta content={shortened} name="twitter:description" />
+    </>
+  );
+};
 
 export default Description;
