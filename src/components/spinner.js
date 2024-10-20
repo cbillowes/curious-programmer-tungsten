353b09dd93f8ab67b5c6df0845f9b69f048762a8
Clarice Bouwer <cbillowes@gmail.com>
2024-10-20 09:59:11 +0400

    use random phrases in spinner

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
+const phrases = [
+  'Optimizing your experience',
+  'Loading the good stuff',
+  'Just a moment',
+  'Unleashing magic',
+  'Chasing dragons',
+  'Conjuring unicorns',
+  'Brewing a potion',
+  'Summoning binary',
+  'Casting spells',
+  'Baking cookies',
+  'Building a bridge',
+  'Painting a masterpiece',
+  'Crafting a masterpiece',
+];
+
 const Spinner = () => {
   return (
     <div className="loader-wrapper fixed top-0 bottom-0 left-0 right-0 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-pink-700 via-pink-800 to-gray-900 z-[9999] text-white text-2xl tracking-tighter">
@@ -9,7 +25,7 @@ const Spinner = () => {
           src={require('@images/unicorn-laptop.webp').default}
         />
         <br />
-        Optimizing your experience
+        {phrases[Math.floor(Math.random() * phrases.length)]}
       </div>
     </div>
   );
