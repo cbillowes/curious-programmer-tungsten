cddf0eb2bffb5036adb7bc8580a5c5812c0ebe2b
Clarice Bouwer <cbillowes@gmail.com>
2024-10-24 23:01:02 +0400

    refactor Firebase service account initialization for improved clarity

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -1,14 +1,13 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount =
-  process.env.NODE_ENV === 'production'
-    ? process.env.FIREBASE_SERVICE_ACCOUNT
-    : require('../../keys/firebase.json');
-
-console.log("NODE_ENV: ", process.env.NODE_ENV);
 
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    privateKey:
+      process.env.NODE_ENV === 'production'
+        ? process.env.FIREBASE_SERVICE_ACCOUNT
+        : require('../../keys/firebase.json'),
+  }),
 });
 
 const db = getFirestore();
