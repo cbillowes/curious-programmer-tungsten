be6961633f7dc33cf53a81f879179fd6cca2686b
Clarice Bouwer <cbillowes@gmail.com>
2024-10-24 23:08:08 +0400

    simplify Firebase service account initialization for consistent environment handling

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -3,10 +3,7 @@ const { getFirestore } = require('firebase-admin/firestore');
 
 initializeApp({
   credential: cert({
-    privateKey:
-      process.env.NODE_ENV === 'production'
-        ? process.env.FIREBASE_SERVICE_ACCOUNT
-        : require('../../keys/firebase.json'),
+    privateKey: process.env.FIREBASE_SERVICE_ACCOUNT,
   }),
 });
 
