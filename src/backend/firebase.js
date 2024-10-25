d421223653d4086adf91d642c8a7990beee77f3a
Clarice Bouwer <cbillowes@gmail.com>
2024-10-25 06:59:14 +0400

    update Firebase initialization to use service account JSON for credentials

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -1,11 +1,15 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
+const { client_email, project_id, private_key } = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT,
+);
+
 initializeApp({
   credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_SERVICE_ACCOUNT,
+    projectId: project_id,
+    clientEmail: client_email,
+    privateKey: private_key,
   }),
 });
 
