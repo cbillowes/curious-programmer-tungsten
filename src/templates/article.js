afbca9f1d1f661e3c65b7cd217955c121633a9b8
Clarice Bouwer <cbillowes@gmail.com>
2024-11-25 07:28:54 +0400

    feat: include share icons on articles

--- src/templates/article.js
+++ src/templates/article.js
@@ -10,6 +10,15 @@ import Backdrop from '@components/backdrop';
 import Subscribe from '@components/subscribe';
 import { getKeywords } from '@common/seo';
 import { StaticImage } from 'gatsby-plugin-image';
+import {
+  FaFacebook,
+  FaHackerNewsSquare,
+  FaLinkedin,
+  FaMastodon,
+  FaRedditSquare,
+  FaWhatsappSquare,
+} from 'react-icons/fa';
+import { FaBluesky, FaSquareThreads, FaTelegram } from 'react-icons/fa6';
 
 export const query = graphql`
   query ArticleTemplateQuery($slug: String!) {
@@ -59,6 +68,10 @@ export const query = graphql`
 const ArticleTemplate = ({ data }) => {
   const { markdownRemark } = data;
   const { timeToRead, html, fields, frontmatter } = markdownRemark;
+  const encodedUrl = encodeURIComponent(
+    `${data.site.siteMetadata.siteUrl}${fields.slug}`,
+  );
+  const encodedTitle = encodeURIComponent(frontmatter.title);
 
   return (
     <Layout
@@ -123,12 +136,107 @@ const ArticleTemplate = ({ data }) => {
                 />
               </article>
             </div>
+            <section className="mt-8 max-w-lg mx-auto">
+              <h2 className="mb-4">Share this article on…</h2>
+              <ul className="flex gap-4">
+                <li>
+                  <a
+                    href={`https://tootpick.org/#text=${encodedTitle}%20-%20${encodedUrl}`}
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"
+                    className="block hover:bg-white rounded-full border-2 border-transparent hover:border-white"
+                  >
+                    <FaMastodon className="text-4xl text-gray-700 dark:text-gray-200 hover:text-[#1877F2] hover:dark:text-[#1877F2]" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={`https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`}
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"
+                    className="block hover:bg-white rounded-full border-2 border-transparent hover:border-white"
+                  >
+                    <FaFacebook className="text-4xl text-gray-700 dark:text-gray-200 hover:text-[#1877F2] hover:dark:text-[#1877F2]" />
+                  </a>
+                </li>
+                <li className="text-white">
+                  <a
+                    href={`https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}`}
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"
+                    className="block hover:bg-white rounded-md border-2 border-transparent"
+                  >
+                    <FaLinkedin className="text-4xl text-gray-700 dark:text-gray-200 hover:text-[#0077B5] hover:dark:text-[#0077B5]" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={`https://bsky.app/intent/compose?text=${encodedTitle}%20-%20${encodedUrl}`}
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"
+                    className="block border-2 border-transparent"
+                  >
+                    <FaBluesky className="text-4xl text-gray-700 dark:text-gray-200 hover:text-[#0085ff] hover:dark:text-[#0085ff]" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={`https://www.threads.net/intent/post?url=${encodedUrl}&amp;text=${encodedTitle}`}
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"
+                    className="block hover:bg-white rounded-md border-2 border-transparent"
+                  >
+                    <FaSquareThreads className="text-4xl text-gray-700 dark:text-gray-200 hover:text-[#000000] hover:dark:text-[#000000]" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={`https://www.reddit.com/submit?url=${encodedUrl}&amp;title=${encodedTitle}`}
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"
+                    className="block hover:bg-white rounded-md border-2 border-transparent"
+                  >
+                    <FaRedditSquare className="text-4xl text-gray-700 dark:text-gray-200 hover:text-[#FF4500] hover:dark:text-[#FF4500]" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={`https://news.ycombinator.com/submitlink?u=${encodedUrl}&amp;t=${encodedTitle}`}
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"
+                    className="block hover:bg-white rounded-md border-2 border-transparent"
+                  >
+                    <FaHackerNewsSquare className="text-4xl text-gray-700 dark:text-gray-200 hover:text-[#fd6600] hover:dark:text-[#fd6600]" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={`https://api.whatsapp.com/send/?text=${encodedUrl}`}
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"
+                    className="block hover:bg-white rounded-md border-2 border-transparent"
+                  >
+                    <FaWhatsappSquare className="text-4xl text-gray-700 dark:text-gray-200 hover:text-[#075E54] hover:dark:text-[#075E54]" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={`https://telegram.me/share/url?url=${encodedUrl}&amp;text=${encodedTitle}`}
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"
+                    className="block hover:bg-white rounded-full border-2 border-transparent hover:border-white"
+                  >
+                    <FaTelegram className="text-4xl text-gray-700 dark:text-gray-200 hover:text-[#24A1DE] hover:dark:text-[#24A1DE]" />
+                  </a>
+                </li>
+              </ul>
+            </section>
           </div>
         </div>
       </div>
       <hr className="border-gray-200 dark:border-gray-700" />
       <section id="subscribe" className="bg-gray-100 dark:bg-gray-800">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ">
+        <div className="py-8 px-4 mx-auto max-w-screen-3xl sm:py-16 lg:px-6 ">
           <div className="mx-auto max-w-screen-md text-center">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
               Subscribe to my newsletter
