e34f74530be0d252f8872cef0260fc4d2636086b
Clarice Bouwer <cbillowes@gmail.com>
2025-08-23 10:35:27 +0400

    remove subscription & newsletters pages & components

--- src/backend/constants.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports.DOMAIN = process.env.DOMAIN;
-module.exports.SENDER_TOKEN = process.env.SENDER_TOKEN;
-module.exports.NEWSLETTER_GROUP_ID = 'avDxqL';
-module.exports.NEWSLETTER_ENDPOINT = new URL('https://api.sender.net/v2/subscribers');
-
